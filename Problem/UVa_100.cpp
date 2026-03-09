d7d9615865d60b3b29727500f106cd1805f82a57
yu <anna0911854770@gmail.com>
2026-03-09 08:18:16 +0000

    3n+1

--- Problem/UVa_100.cpp
+++ Problem/UVa_100.cpp
@@ -1,1 +1,33 @@
 #include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    while (cin >> a>>b) {
+        int counter = 1;
+        int max = 0;
+        cout << a << " " << b << " ";
+        if (a > b) {
+            swap(a, b);
+        }
+        for (int i = a; i <= b; i++) {
+            int n = i;
+            counter = 1;
+            while (n != 1) {
+                if (n % 2 == 1) {
+                    n = 3 * n + 1;
+                    counter++;
+                    
+                }
+                else {
+                    n = n / 2;
+                    counter++;
+                }
+            }
+            if (counter > max) {
+                max = counter;
+                
+            }
+        }
+        cout << max << endl;
+    }
+}
